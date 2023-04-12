7fd9fa8dd961f4babcf5f09746ad874b744f1975
Tim Wilbrink <tim@picoreo.com>
2023-04-12 07:58:00 +0200

    Workaround for MF selection

--- Sources/NFCPassportReader/TagReader.swift
+++ Sources/NFCPassportReader/TagReader.swift
@@ -222,6 +222,15 @@ public class TagReader {
         // By executing above SELECT command (with data=0x3F00) master file should be selected and you should be able to read EF.CardAccess from passport.
         
         // First select master file
+        let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x04, p2Parameter: 0x0C, data: Data([0xF0, 0x00, 0x00, 0x00, 0x03]), expectedResponseLength: -1)
+        do {
+            _ = try await send( cmd: cmd)
+        } catch NFCPassportReaderError {
+            Log.info("Could not select MF at F000000003. Next try 3F00.")
+        }
+        
+        
+        // First select master file
         let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x00, p2Parameter: 0x0C, data: Data([0x3f,0x00]), expectedResponseLength: -1)
         
         _ = try await send( cmd: cmd)
